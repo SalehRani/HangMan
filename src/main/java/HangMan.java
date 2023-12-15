57b0772d1bdc84dce749a16a4dab781f691d0b5b
Rani Saleh <xx.rani.saleh.xx@hotmail.com>
2023-12-15 15:33:32 -0600

    Added functionality where the game will prompt you to play again. Running into an issue where if you guess correctly twice in a row, it ends the program.

--- src/main/java/HangMan.java
+++ src/main/java/HangMan.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -11,12 +12,15 @@ public class HangMan {
 
         Scanner keyboard = new Scanner(System.in);
         // Takes in account the length of words array, and randomly generates the index of the word to choose from the array.
-        String wordToGuess = WORDS[(int) (Math.random() * WORDS.length)];
-        char[] guessedWord = new char[wordToGuess.length()];
+        String wordToGuess;
+        char[] guessedWord;
         Map<Character, Boolean> guessedLetters = new HashMap<>();
         char[] letterArr = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         int tries = 0;
         boolean wordGuessed = false;
+        boolean gameOver = false;
+        wordToGuess = WORDS[(int) (Math.random() * WORDS.length)];
+        guessedWord = new char[wordToGuess.length()];
 
         // Populating Map with every letter initially false
         for (int i = 0; i < 26; i++) {
@@ -27,6 +31,7 @@ public class HangMan {
         for (int i = 0; i < wordToGuess.length(); i++) {
             guessedWord[i] = '_';
         }
+
         System.out.println();
         System.out.println("---------------------------------------");
         System.out.println("         Welcome to Hangman!");
@@ -34,6 +39,7 @@ public class HangMan {
         System.out.println();
 
         while (!wordGuessed && tries < MAX_TRIES) {
+
             if (tries == 5) {
                 System.out.println("         -------------------");
                 System.out.println("         -------------------");
@@ -161,12 +167,15 @@ public class HangMan {
                 System.out.println("------------------");
                 System.out.println("------------------");
             }
+
             System.out.println("\nCurrent word: " + new String(guessedWord));
             System.out.println("Tries left: " + (MAX_TRIES - tries));
             System.out.print("Enter a letter: ");
             String guessString = keyboard.nextLine();
+            System.out.println();
             char guess;
-            if (guessString.length() > 0) {
+
+            if (!guessString.isEmpty()) {
                 guess = guessString.toLowerCase().charAt(0);
             } else {
                 System.out.println("\nPlease guess the letter or the entire word.\n");
@@ -176,22 +185,22 @@ public class HangMan {
             if (guessString.equals(wordToGuess)) {
                 wordGuessed = true;
             }
-            if (!guessString.equals(wordToGuess) && guessString.length() > 1) {
-                System.out.println("Please guess one letter at a time unless you're ready to guess the full word. ");
-                continue;
-            }
 
-            if (guessedLetters.get(guess)) {
-                System.out.println("You've already guessed this letter. Try another one.");
+            if (guessedLetters.get(guess) && !guessString.equals(wordToGuess)) {
+                System.out.println("You've already guessed this letter. Try another one.\n");
             }
 
             guessedLetters.put(guess, true);
             boolean found = false;
 
-            for (int i = 0; i < wordToGuess.length(); i++) {
-                if (wordToGuess.charAt(i) == guess) {
-                    guessedWord[i] = guess;
-                    found = true;
+            if (!guessString.equals(wordToGuess) && guessString.length() > 1) {
+                System.out.println("Please guess one letter at a time unless you're ready to guess the full word.\n");
+            } else {
+                for (int i = 0; i < wordToGuess.length(); i++) {
+                    if (wordToGuess.charAt(i) == guess) {
+                        guessedWord[i] = guess;
+                        found = true;
+                    }
                 }
             }
 
@@ -206,32 +215,106 @@ public class HangMan {
                 wordGuessed = true;
             }
 
-        }
+            if (wordGuessed) {
 
-        if (wordGuessed) {
-            System.out.println("\nCongratulations! You've guessed the word: " + wordToGuess);
-        } else {
-            System.out.println("         -------------------");
-            System.out.println("         -------------------");
-            System.out.println("         ||               |");
-            System.out.println("         ||               |");
-            System.out.println("         ||             ^^^^^");
-            System.out.println("         ||            ( 0_0 )");
-            System.out.println("         ||              | |");
-            System.out.println("         ||          (-       -)");
-            System.out.println("         ||          | |     | |");
-            System.out.println("         ||          | |     | |");
-            System.out.println("         ||          ( )=====( )");
-            System.out.println("         ||            | |  | |");
-            System.out.println("         ||            | |  | |");
-            System.out.println("         ||            | |  | |");
-            System.out.println("         ||            | |  | |");
-            System.out.println("         ||            (_)  (_)");
-            System.out.println("         ||");
-            System.out.println("         ||");
-            System.out.println("------------------");
-            System.out.println("------------------");
-            System.out.println("\nSorry, you've run out of tries. The word was: " + wordToGuess);
+                // Populating Map with every letter initially false
+                for (int i = 0; i < 26; i++) {
+                    guessedLetters.put(letterArr[i], false);
+                }
+
+                // Initialize guessedWord with dashes
+                for (int i = 0; i < wordToGuess.length(); i++) {
+                    guessedWord[i] = '_';
+                }
+
+                System.out.println("Congratulations! You've guessed the word: " + wordToGuess);
+
+                while (!gameOver) {
+                    System.out.print("\nWould you like to play again? (Y/N): ");
+
+                    String choice = keyboard.nextLine();
+
+                    if (choice.equalsIgnoreCase("Y")) {
+                        wordGuessed = false;
+                        tries = 0;
+                        guessedLetters = new HashMap<>();
+                        wordToGuess = WORDS[(int) (Math.random() * WORDS.length)];
+                        guessedWord = new char[wordToGuess.length()];
+
+                        // Populating Map with every letter initially false
+                        for (int i = 0; i < 26; i++) {
+                            guessedLetters.put(letterArr[i], false);
+                        }
+
+                        // Initialize guessedWord with dashes
+                        for (int i = 0; i < wordToGuess.length(); i++) {
+                            guessedWord[i] = '_';
+                        }
+
+                        System.out.println();
+                        gameOver = true;
+                    } else if (choice.equalsIgnoreCase("N")) {
+                        System.out.println("\nThank you for playing!");
+                        System.exit(0);
+                    } else {
+                        System.out.println("Please enter 'Y' or 'N' to make a choice.");
+                    }
+                }
+
+            } else if (tries == 6) {
+                tries = 0;
+                guessedLetters = new HashMap<>();
+                wordToGuess = WORDS[(int) (Math.random() * WORDS.length)];
+                guessedWord = new char[wordToGuess.length()];
+
+                System.out.println("         -------------------");
+                System.out.println("         -------------------");
+                System.out.println("         ||               |");
+                System.out.println("         ||               |");
+                System.out.println("         ||             ^^^^^");
+                System.out.println("         ||            ( 0_0 )");
+                System.out.println("         ||              | |");
+                System.out.println("         ||          (-       -)");
+                System.out.println("         ||          | |     | |");
+                System.out.println("         ||          | |     | |");
+                System.out.println("         ||          ( )=====( )");
+                System.out.println("         ||            | |  | |");
+                System.out.println("         ||            | |  | |");
+                System.out.println("         ||            | |  | |");
+                System.out.println("         ||            | |  | |");
+                System.out.println("         ||            (_)  (_)");
+                System.out.println("         ||");
+                System.out.println("         ||");
+                System.out.println("------------------");
+                System.out.println("------------------");
+                System.out.println("\nSorry, you've run out of tries. The word was: " + wordToGuess);
+
+                while (!gameOver) {
+                    System.out.print("\nWould you like to play again? (Y/N): ");
+
+                    String choice = keyboard.nextLine();
+
+                    if (choice.equalsIgnoreCase("Y")) {
+                        // Populating Map with every letter initially false
+                        for (int i = 0; i < 26; i++) {
+                            guessedLetters.put(letterArr[i], false);
+                        }
+
+                        // Initialize guessedWord with dashes
+                        for (int i = 0; i < wordToGuess.length(); i++) {
+                            guessedWord[i] = '_';
+                        }
+
+                        gameOver = true;
+
+                    } else if (choice.equalsIgnoreCase("N")) {
+                        System.out.println("\nThank you for playing!");
+                        System.exit(0);
+                    } else {
+                        System.out.println("Please enter 'Y' or 'N' to make a choice.");
+                    }
+                }
+            }
         }
     }
 }
